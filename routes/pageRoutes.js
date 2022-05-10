149bee9ab7f95908aa63fb4eacea218f43ef2f26
rachelleBoeuf <rachelnleboeuf@gmail.com>
2022-05-10 12:34:52 -0600

    all the changes that make awesome!

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    console.log('here');
     try {
         res.render('home');
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.post('/submit', async (req, res) => {
+    try {
+        res.status(200).json({ success: true });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
